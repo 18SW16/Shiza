902a146124a1203282525b0bb8a468cd21df1653
18SW16 <49429420+18SW16@users.noreply.github.com>
2019-04-09 00:37:37 -0700

    mariamjawaid.wordpress.com

---
+++ lab1/Task1.java
@@ -0,0 +1,17 @@
+public class Task1{
+public static void main(String args[])
+{
+String Name="shiza";
+String Rollno="18sw16";
+int TotalMarks=800;
+float ObtainedMarks =500;
+float percentage=(ObtainedMarks/TotalMarks)*100;
+char Grade='B';
+System.out.println("Name: "+ Name);
+System.out.println(" Roll No. : "+ Rollno);
+System.out.println(" Total Marks "+ TotalMarks);
+System.out.println("Obtained Marks : "+ ObtainedMarks);
+System.out.println("Percentage : "+ percentage);
+System.out.println("Grade : PASS"+ Grade);
+}
+}
