90df882a1894ed47111beea58e2ac015cdba57e3
18SW16 <49429420+18SW16@users.noreply.github.com>
2019-07-17 02:04:46 -0700

    Add files via upload

---
+++ lab7/Lab7Task1.java
@@ -0,0 +1,37 @@
+ Lab7Task1{
+
+
+
+public static void main(String args[]){
+
+
+
+Cylinder c1=new Cylinder();
+
+
+
+System.out.println("Radius is " + c1.getRadius());
+
+
+
+System.out.println("Height is " + c1.getHeight());
+
+
+
+System.out.println("Color is " + c1.getColor());
+
+
+
+System.out.println("Area is " + c1.getArea());
+
+
+
+System.out.println("Volume is " + c1.getVolume());
+
+
+
+}
+
+
+
+}
---
+++ lab7/lab7Task2.java
@@ -0,0 +1,33 @@
+ Lab7Task2{
+
+
+
+public static void main(String args[]){
+
+
+
+Person p=new Person("shiza" , "house no.45 Block a1, Hyderabad");
+
+
+
+Student s=new Student("shiza","xyz","Software Engineering",1,4000);
+
+
+
+Staff s1=new Staff("shiza","block C1, unit no.6","ZS",2000);
+
+
+
+System.out.println(s.toString());
+
+
+
+System.out.println(s1.toString());
+
+
+
+  }
+
+
+
+}
